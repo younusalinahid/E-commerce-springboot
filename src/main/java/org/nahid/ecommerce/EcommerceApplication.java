e222943a752629c3d7958f77581f42315b6e6054
nahid <eng.yanahid@gmail.com>
2024-05-27 02:07:41 +0600

    Create Ecommerce springboot project

---
+++ src/main/java/org/nahid/ecommerce/EcommerceApplication.java
@@ -0,0 +1,12 @@
+package org.nahid.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+}
